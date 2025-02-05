8266951d26aaa71bce95c24db40e193d14419da5
RichardMorris <richardmorris@truro-penwith.ac.uk>
2025-02-05 16:56:00 +0000

    add echo client

--- networking/echo_client.py
+++ networking/echo_client.py
@@ -1,23 +1,40 @@
 # Import socket module
 import socket
+import sys
 
-# Create a socket object
-s = socket.socket()
+def open_socket():
+    global ip_addr,sock
+    # Create a socket object
+    sock = socket.socket()
 
-# Define the port on which you want to connect
-port = 40674
+    # Define the port on which you want to connect
+    port = 40674
 
-# connect to the server on local computer
-s.connect(('127.0.0.1', port))
-name = input("Please type your name ")
+    # connect to the server on 
+    sock.connect((ip_addr, port))
 
-print("Sending datas")
 
-s.send(name.encode())
 
-print("Data sent")
-# receive data from the server
-print(s.recv(1024))
+if __name__ == '__main__':
+    ip_addr = sys.argv[1]
+    print("Connecting to ",ip_addr)
+    open_socket()
+    print("Connected")
 
-# close the connection
-s.close()
+    name = input("Please type your name ")
+    print("Sending:",name)
+    sock.send(name.encode())
+    print("Data sent")
+
+    # receive data from the server
+    while True:
+        data = sock.recv(1024)
+        print("Received: ",len(data)," bytes")
+        if(len(data)==0):
+            break
+        print("Received: ",data.decode())
+
+    # close the connection
+    print("Closing connection")
+    sock.close()
+    print("closed")
